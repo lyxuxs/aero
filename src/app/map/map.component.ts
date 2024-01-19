b7951265fd8b80da9fa8ed387375ae09cc50e367
mipunaruwan <nipunakalpa1234@gmail.com>
2024-01-19 09:45:32 +0530

    changed icon marker

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,21 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrls: ['./map.component.css']
 })
 export class MapComponent {
   constructor() { }
-  ngOnInit(): void { }
+  
   center: google.maps.LatLngLiteral = {
     lat: 51.165691,
     lng: 10.451526
   };
+  
   zoom = 4;
-  markerOptions: google.maps.MarkerOptions = {
-    draggable: false
-  };
+  
   markerPositions: google.maps.LatLngLiteral[] = [];
+  markerOptions: google.maps.MarkerOptions[] = []; // Add this line
+  
   addMarker(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.markerPositions.push(event.latLng.toJSON());
+    if (event.latLng != null) {
+      const newMarkerPosition = event.latLng.toJSON();
+      
+      // Create a new marker options object for each marker
+      const newMarkerOptions: google.maps.MarkerOptions = {
+        position: newMarkerPosition,
+        draggable: false,
+        icon: '../../assets/icons/icons8-construction-100.png' // Set the icon URL here
+      };
+      
+      this.markerPositions.push(newMarkerPosition);
+      this.markerOptions.push(newMarkerOptions);
+    }
   }
 }
